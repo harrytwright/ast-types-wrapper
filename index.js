9a549202ab25cf8e485f91d459946d8bf1a1ed03
harrytwright <haroldtomwright@gmail.com>
2021-08-20 17:09:36 +0100

    src: Add basic exporter
    Signed-off-by: harrytwright <haroldtomwright@gmail.com>

---
+++ index.js
@@ -0,0 +1,10 @@
+// Basic export
+
+const utils = require('./src/utils')
+const object = require('./src/object')
+const variable = require('./src/variable')
+const functions = require('./src/functions')
+
+module.exports = {
+  utils, object, variable, functions
+}
